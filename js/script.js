93564c4bcfe23359a940c6bb626ed37ccc1bb9d2
atsi87 <atsimam@gmail.com>
2022-02-16 19:49:28 -0800

    my new js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 
-const answers = document.getElementById('answers');
-// console.log(answers);
-answers.addEventListener('click', function ask() {
-    console.log('random number');
-})
+const answer = document.getElementById("answer");
+const questionButton = document.getElementById('questionButton');
+questionButton.addEventListener("click", function () {
+    const magicball = getRandomIntInclusive(1, 20);
+    console.log(magicball);
+answer.src = `../img/magic8ball_${magicball}.png`;
+});
+
+function getRandomIntInclusive(min, max) {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue); 
+}
+
 
 
